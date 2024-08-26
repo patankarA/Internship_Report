2aab207ea72a529a4bff187336108006160257a6
patankarA <146120422+patankarA@users.noreply.github.com>
2024-08-26 08:26:28 +0530

    Create Problem4_OptiApp.cpp

---
+++ Problem4_OptiApp.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int zw[4][20] = {0}, zr[4][20] = {0};
+    zw[0][0]=1;
+    zw[1][0]=2;
+    zw[2][0]=3;
+    zw[3][0]=4;
+    while(1)
+    {
+        cout<<"enter a string"<<endl;
+        char ch[20];
+        cin>>ch;
+        int t = ch[1]-48, dist = ch[4]-48, x=ch[3]-'p', u;
+        if(zr[ch[0]-'p'][t]==1)
+        {
+            cout<<"error"<<endl;
+            break;
+        }
+        if(ch[3]-48 < 64 )
+        {
+            u=ch[3]-48;
+        }
+        else
+        {
+            while(zw[x][dist]==0)
+            {
+                zr[x][dist]=1;
+                dist--;
+            }
+            u=zw[x][dist];
+        }
+        zw[ch[0]-'p'][t] = u;
+        
+        
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<20;j++)
+            {
+                cout<<zw[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    } 
+    return 0;
+}
+
+
